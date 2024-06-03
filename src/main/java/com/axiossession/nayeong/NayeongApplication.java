c1fcab457caec8441ac743be278a299299cc0a8c
nayeong lee <lny021102@gmail.com>
2024-06-03 15:51:49 +0900

    [INIT] initial setting

---
+++ src/main/java/com/axiossession/nayeong/NayeongApplication.java
@@ -0,0 +1,13 @@
+package com.axiossession.nayeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NayeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NayeongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axiossession/nayeong/NayeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axiossession.nayeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NayeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
